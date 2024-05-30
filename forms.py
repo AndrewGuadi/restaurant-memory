509ec32b25cc659a71dfced65387ccf64ff0bf3c
Andrew Guadí <94252760+AndrewGuadi@users.noreply.github.com>
2024-05-30 00:34:07 +0000

    adding testig for cards

--- forms.py
+++ forms.py
@@ -1,10 +1,22 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SelectField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError
 
 class FlashcardForm(FlaskForm):
-    dish = StringField('Dish', validators=[DataRequired()])
-    beverage = StringField('Beverage', validators=[DataRequired()])
-    ingredients = TextAreaField('Ingredients', validators=[DataRequired()])
     category = SelectField('Category', choices=[('Bar Beverages', 'Bar Beverages'), ('Appetizers', 'Appetizers'), ('Main Dishes', 'Main Dishes'), ('Desserts', 'Desserts'), ('Sides', 'Sides')], validators=[DataRequired()])
+    dish = StringField('Dish')
+    beverage = StringField('Beverage')
+    ingredients = TextAreaField('Ingredients', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
+    def validate(self):
+        rv = FlaskForm.validate(self)
+        if not rv:
+            return False
+        
+        if not self.dish.data and not self.beverage.data:
+            self.dish.errors.append('Either Dish or Beverage must be filled out.')
+            self.beverage.errors.append('Either Dish or Beverage must be filled out.')
+            return False
+
+        return True
